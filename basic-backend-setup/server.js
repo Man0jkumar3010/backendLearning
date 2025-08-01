9c4fa60041a987c8f5c6ed04be6f381eeb572899
Manoj <manojkumar.fsd3@gmail.com>
2025-08-01 10:29:56 +0530

    Remove the consoles and command lines

--- basic-backend-setup/server.js
+++ basic-backend-setup/server.js
@@ -31,36 +31,32 @@ app.get('/api/users', (req, res) => {
   res.send(data);
 });
 
-app.get('/api/user', (req, res) => {
-  const {id} = req.query;
-  console.log("QueryData ====>", parseInt(id));
-  const filteredUsers = data.filter(user => user.id === parseInt(id));    
+app.get("/api/user", (req, res) => {
+  const { id } = req.query;
+  const filteredUsers = data.filter((user) => user.id === parseInt(id));
   res.json(filteredUsers);
 });
 
-app.post('/api/user', (req, res) => {
+app.post("/api/user", (req, res) => {
   const userData = req.body;
-  console.log("Post data ====>", data.push(userData));
   res.send(userData);
 });
 
-app.delete('/api/users',(req,res)=>{
-    const deleteData = data.pop()
-    res.send(deleteData)
-})
+app.delete("/api/users", (req, res) => {
+  const deleteData = data.pop();
+  res.send(deleteData);
+});
 
-app.delete('/api/user', (req, res) => {
+app.delete("/api/user", (req, res) => {
   const { id } = req.query;
   const userId = parseInt(id);
-
   const originalLength = data.length;
-
-  data = data.filter(user => user.id !== userId);
+  data = data.filter((user) => user.id !== userId);
 
   if (data.length < originalLength) {
     res.json({
       message: `User with ID ${userId} deleted successfully`,
-      remainingUsers: data
+      remainingUsers: data,
     });
   } else {
     res.status(404).json({ message: `User with ID ${userId} not found` });
